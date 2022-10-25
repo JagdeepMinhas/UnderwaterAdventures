123308161419dd0e4c5d4154cea9e0cbbf158d08
Hazelle Lebumfacil <hlebumfa@sfu.ca>
2022-10-25 03:54:01 -0700

    Feat: static variables included for 50 x 50 gameboard

--- underwateradventures/src/test/java/com/group19/gameBoard.java
+++ underwateradventures/src/test/java/com/group19/gameBoard.java
@@ -3,7 +3,17 @@ package test.java.com.group19;
 import javax.swing.JPanel;
 
 
-
 public class gameBoard extends JPanel{
     
+    //cell size in characters (arbitrary)
+    final int cellSize = 40;
+    
+    // 50x50 gameBoard
+    final int maxCol = 50;
+    final int maxRow = 50;
+    final int screenWidth = maxCol * cellSize;
+    final int screenHeight = maxRow * cellSize;
+
+    
+
 }
